cde342642b85c97c6e28d49873b5c26cd34faff1
Joseph cohen <jojocohen57@gmail.com>
2020-09-23 21:42:30 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  root 'static#home'
+  resources :users, :attractions, :users, :rides, :sessions 
+  get '/signin', to: 'session#new', as: 'signin'
+end 
+  
+  
