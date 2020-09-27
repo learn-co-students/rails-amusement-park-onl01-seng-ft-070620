f13f98c648a0683a989d109b0d838bf84be0b692
Directrix777 <andrew.j.ribas@gmail.com>
2020-09-27 18:50:28 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,18 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :attraction
+    def take_ride
+        if user.height < attraction.min_height && user.tickets < attraction.tickets
+            return "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
+        elsif user.height < attraction.min_height
+            return "Sorry. You are not tall enough to ride the Roller Coaster."
+        elsif user.tickets < attraction.tickets
+            return "Sorry. You do not have enough tickets to ride the Roller Coaster."
+        else
+            user.nausea = user.nausea + attraction.nausea_rating
+            user.happiness = user.happiness + attraction.happiness_rating
+            user.tickets = user.tickets - attraction.tickets
+            user.save
+        end
+    end
 end
