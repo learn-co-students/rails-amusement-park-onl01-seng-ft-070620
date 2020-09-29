15ca6decc90d121234013d395ce7e72732af6740
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-09-29 18:11:27 -0500

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Attraction < ApplicationRecord
+  has_many :rides
+  has_many :users, through: :rides
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require "capybara/rspec"
-require "capybara/dsl"
+require 'capybara/rspec'
+require 'capybara/dsl'
 require 'database_cleaner'
-require "rack_session_access/capybara"
+require 'rack_session_access/capybara'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -32,7 +34,6 @@ require "rack_session_access/capybara"
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
@@ -42,7 +43,7 @@ RSpec.configure do |config|
     DatabaseCleaner.start
   end
 
-  config.before(:each, :js => true) do
+  config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
 
@@ -50,7 +51,7 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
   config.include Capybara::DSL
-  config.include LoginHelper, :type => :feature
+  config.include LoginHelper, type: :feature
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -79,6 +80,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-
 end
